d09e8aa1e3bded91e5f6e7464da9d2e606851361
Kelson Warner <kelson@null.net>
2019-07-21 10:49:57 -0700

    adding prettier and eslint

---
+++ static-testing/src/example.js
@@ -0,0 +1,11 @@
+const named = 'Freddy';
+typeof named === 'string';
+
+if (!('serviceWorker' in navigator)) {
+  // you have old browser
+}
+
+const greeting = 'hello';
+console.log(`${greeting} world`);
+
+[(1, 2, 3)].forEach(x => console.log(x));
